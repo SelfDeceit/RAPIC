aab0de4f765f05d99dcf39d5e7ac6f6b68ea313f
Anegan <nesanyorku@gmail.com>
2015-04-10 23:49:15 -0700

    champion_data.php
    done, maybe add more things later but good for now

--- php/champion_data.php
+++ php/champion_data.php
@@ -1,12 +1,14 @@
 <?php
 	include("static_var.php");
-	
+	//Formats all champion information into a multidimensional array
 	$CHAMPION_INFO = json_decode(file_get_contents('https://na.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=&api_key=' . $API_KEY), true);
 	
-	foreach ($CHAMPION_INFO['data'] as $champion)
+	$CHAMPION_API_KEY_NAMES = array();
+	//Adds each champion to the array
+	foreach ($CHAMPION_INFO['data'] as $key => $val)
 	{
-		echo $champion['id'] . "</br>";
+		array_push($CHAMPION_API_KEY_NAMES, $key); 
 	}
-	
+	sort($CHAMPION_API_KEY_NAMES); //Sort the array alphabetically
 ?>
 
